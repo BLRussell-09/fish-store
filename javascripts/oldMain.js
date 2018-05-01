97a9b1cdc3cd4761c95ac442b22e76770dab3d0f
Barry Russell <blrussell09@gmail.com>
2018-05-01 18:20:28 -0500

    Added jquery to eslintrc

---
+++ javascripts/oldMain.js
@@ -0,0 +1,40 @@
+// Filter fish that are "on sale"
+
+const filterFish = () =>
+{
+  $('#available .fish').not('.on-sale').toggle();
+};
+
+const buttonText = () =>
+{
+  $('#show-sale').text((i, text) =>
+  {
+    let returnText = '';
+    if (text === 'Show Sale Fish')
+    {
+      returnText = 'Show All';
+    }
+    else
+    {
+      returnText = 'Show Sale Fish';
+    }
+    return returnText;
+  });
+};
+
+$('#show-sale').click(() =>
+{
+  buttonText();
+  filterFish();
+});
+
+// Add fish to "Basket"
+
+const moveToCart = (e) =>
+{
+  const fishCard = $(e.target).closest('.fish');
+  console.log(fishCard);
+  $('#snagged').append(fishCard);
+};
+
+$('button.add').click(moveToCart);
