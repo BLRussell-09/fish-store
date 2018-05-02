9cae30e82a9c836afada3391e196cb15304aeba2
Barry Russell <blrussell09@gmail.com>
2018-05-01 19:48:44 -0500

    Added button Event listeners

--- javascripts/events.js
+++ javascripts/events.js
@@ -33,7 +33,7 @@ const moveToCart = (e) =>
 
 const bindEvents = () =>
 {
-  $('button.add').click(moveToCart);
+  $('button.add').on('click', moveToCart);
   $('#show-sale').click(() =>
   {
     buttonText();
