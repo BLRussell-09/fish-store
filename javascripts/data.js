485c6c7936f93f98c0abc55f252e94b359bc3ce1
Barry Russell <blrussell09@gmail.com>
2018-05-01 20:50:43 -0500

    Added discount

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,12 +1,14 @@
 const loadFishes = require('./fishes');
 const writeFishes = require('./fishDom');
 const events = require('./events');
+const applySale = require('./discount');
 
 const iLoad = (data) =>
 {
   console.log(data);
   $('#available').append(writeFishes(data.fishes));
   events();
+  applySale();
 };
 
 const iFail = () =>
